d3d60cf9244db0e08c91981d68006b5618e6c66e
Mark <Mark@MarksMBP.local>
2017-05-10 15:45:04 +0800

    fetch

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -7,7 +7,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
   
   @doc """
@@ -26,6 +28,16 @@ defmodule Issues.CLI do
       {_, [user, project], _} -> {user, project, @default_count}
       _ -> :help
     end
+  end  
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [count | #{@default_count}]
+    """
   end
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
+  
   
 end
